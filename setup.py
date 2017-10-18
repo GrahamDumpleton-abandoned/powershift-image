3eb17f5c55d55f8cdb1a65b2ec5f54d2ecf5b1c8
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-10-18 17:19:35 +1100

    Increment version to 1.1.1.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
 
 setup_kwargs = dict(
     name='powershift-image',
-    version='1.1.0',
+    version='1.1.1',
     description='PowerShift command plugin for working in S2I images.',
     long_description=long_description,
     url='https://github.com/getwarped/powershift-image',
